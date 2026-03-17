38f3681b0bd4b3c5e7493d45cabf4e799d2b6b37
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-17 23:17:46 +0500

    blocking and non-blocking

--- Hello-Word/file.js
+++ Hello-Word/file.js
@@ -1,10 +1,15 @@
 const { log } = require("console")
 const fs = require("fs")
-//sync....
+
+const os = require('os')
+
+console.log(os.cpus().length);
+
+//sync....  blocking....
 
 // fs.writeFileSync('./test.txt' , 'hey there')  
 
-//Async....
+//Async.... non-blcking request      
 
 // fs.writeFile('./test.txt' , 'hey haris',(err) => { })
 
@@ -22,15 +27,44 @@ const fs = require("fs")
 // })
 
 
-fs.appendFileSync('./test.txt' , `${Date.now()}hey there\n` )
+// fs.appendFileSync('./test.txt' , `${Date.now()}hey there\n` )
 
-// fs.cpSync('./test.txt','./copy.txt')
+// // fs.cpSync('./test.txt','./copy.txt')
 
-// fs.unlinkSync('./copy.txt')
+// // fs.unlinkSync('./copy.txt')
 
-console.log(fs.statSync('./test.txt'));
-
-
+// console.log(fs.statSync('./test.txt'));
 
 
-fs.mkdirSync('my-docs')
+
+
+// fs.mkdirSync('my-docs')
+
+
+// ......................................new lec/................
+
+// console.log('1'); 
+
+// blocking
+
+// const result = fs.readFileSync('./test.txt','utf-8')
+// console.log(result);
+
+
+// console.log('3');
+// console.log('4');
+// console.log('7');
+
+
+// non- blocking
+
+fs.readFile('./test.txt','utf-8', (err,result)=>{
+    //  console.log(result);
+})
+// console.log('3');
+// console.log('4');
+//  console.log('7');
+ 
+//  default thread poool size = 4
+
+// max?  8core cpu =8
