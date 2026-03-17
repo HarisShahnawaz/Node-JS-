9b768715b48131a0f9a5913415b7e69aac5df835
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-18 01:08:07 +0500

    http server created and downloadedd node  modules for url

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,35 @@
 const http = require('http')
 const fs = require('fs')
-
- 
+const url = require('url')
+   
  
 const mySever = http.createServer((req,res)=>{
+    if (req.url === "/favicon.ico") return res.end
 
-    const log = `${Date.now()}: New request Recieved\n`
+    const log = `${Date.now()}: ${req.url} New request Recieved\n`
+
+    const myUrl = url.parse(req.url , true)
+    console.log(myUrl);
+    
      fs.appendFile('log.txt',log,(err,data)=>{
-         res.end('Hello From Server Again'); 
+
+         switch (myUrl.pathname) {
+            case  ('/'): 
+                 res.end('Homepage'); 
+                break;
+                case  ('/about'): 
+                const username = myUrl.query.myname
+                 res.end(`Hi,${username}`); 
+                break;
+                case ("/search"):
+                const search=  myUrl.query.search_query
+                res.end("Here are your results for" +search )
+            default:
+                res.end('Error 404 Not Found'); 
+                break;
+         }
+
+        
      })
      
 })
