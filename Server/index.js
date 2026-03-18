ea57207af9b1c51271300c32785caec90a556f86
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-18 20:50:24 +0500

    http requests: get post put patch delete

--- Server/index.js
+++ Server/index.js
@@ -6,16 +6,18 @@ const url = require('url')
 const mySever = http.createServer((req,res)=>{
     if (req.url === "/favicon.ico") return res.end
 
-    const log = `${Date.now()}: ${req.url} New request Recieved\n`
+    const log = `${Date.now()}: ${req.method} ${req.url} New request Recieved\n`
 
     const myUrl = url.parse(req.url , true)
-    console.log(myUrl);
+
     
      fs.appendFile('log.txt',log,(err,data)=>{
 
          switch (myUrl.pathname) {
             case  ('/'): 
-                 res.end('Homepage'); 
+               if (req.method === "Get") {
+                res.end('Home page'); 
+               }  
                 break;
                 case  ('/about'): 
                 const username = myUrl.query.myname
@@ -24,6 +26,17 @@ const mySever = http.createServer((req,res)=>{
                 case ("/search"):
                 const search=  myUrl.query.search_query
                 res.end("Here are your results for" +search )
+                case ("/signup"):
+                    if (req.method === "GET")
+                         {
+                res.end('this is a signup form');
+               } 
+               else if(req.method === "POST"){
+                //db query
+                res.end('success');
+               }
+               break;
+
             default:
                 res.end('Error 404 Not Found'); 
                 break;
