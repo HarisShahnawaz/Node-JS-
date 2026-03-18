03866654c86fd59ff81de691603e1ddef472c413
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-18 21:59:20 +0500

    express js setup

--- Server/index.js
+++ Server/index.js
@@ -1,51 +1,13 @@
-const http = require('http')
-const fs = require('fs')
-const url = require('url')
-   
- 
-const mySever = http.createServer((req,res)=>{
-    if (req.url === "/favicon.ico") return res.end
+const express = require('express')
 
-    const log = `${Date.now()}: ${req.method} ${req.url} New request Recieved\n`
+const app = express();
 
-    const myUrl = url.parse(req.url , true)
-
-    
-     fs.appendFile('log.txt',log,(err,data)=>{
-
-         switch (myUrl.pathname) {
-            case  ('/'): 
-               if (req.method === "Get") {
-                res.end('Home page'); 
-               }  
-                break;
-                case  ('/about'): 
-                const username = myUrl.query.myname
-                 res.end(`Hi,${username}`); 
-                break;
-                case ("/search"):
-                const search=  myUrl.query.search_query
-                res.end("Here are your results for" +search )
-                case ("/signup"):
-                    if (req.method === "GET")
-                         {
-                res.end('this is a signup form');
-               } 
-               else if(req.method === "POST"){
-                //db query
-                res.end('success');
-               }
-               break;
-
-            default:
-                res.end('Error 404 Not Found'); 
-                break;
-         }
-
-        
-     })
-     
+app.get('/', (req,res)=>{
+    res.send('Hello from home page' )
+})
+app.get('/about', (req,res)=>{
+    res.send(`hello ${req.query.name}`)
 })
 
 
-mySever.listen(8000,()=> console.log("Server Started"));
+app.listen(8000, ()=> console.log("Server Started!"))
