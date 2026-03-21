b3e76cac7219c2adbda2c6be3908a700afd343e5
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-21 23:30:30 +0500

    make our /users route

--- Project-01/index.js
+++ Project-01/index.js
@@ -5,8 +5,19 @@ const PORT = 8000;
 
 // Routes
 
-app.get("/users", (req,res)=> {
+app.get("/api/users", (req,res)=> {
      return  res.json(users)
+})
+
+app.get('/users' , (req,res)=> {
+    const html =
+    ` <ul>
+      ${users.map(user => `<li>${user.first_name}<li>`).join("")}
+    <ul>
+    `
+
+
+    return res.send(html)
 })
 
 app.listen(PORT,() => console.log(`Server Started at Port ${PORT}`))
