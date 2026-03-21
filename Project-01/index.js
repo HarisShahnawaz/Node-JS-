38216622f4e95dee73d0d42638908e9040bd8f58
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-22 00:08:27 +0500

    create restfull api

--- Project-01/index.js
+++ Project-01/index.js
@@ -18,11 +18,31 @@ app.get('/users' , (req,res)=> {
 app.get("/api/users", (req,res)=> {
      return  res.json(users)
 })
-app.get("/api/users/:id", (req,res)=> {
+app.route('/api/users/:id')
+.get((req,res)=> {
      const id = Number(req.params.id)
      const user = users.find((user)=> user.id===id)
      return res.json(user)
 })
+.patch((req,res=>{
+     //Todo :  edit user with id
+     return res.json({status: "pending"})
+
+}))
+.delete((req,res=>{
+       //Todo :  delete user with id
+     return res.json({status: "pending"})
+}))
+
+
+
+
+
+app.post("/api/users" , (req,res)=> {
+    //TODO : create new users
+    return res.json({status : "pending"})
+})
+
 
 
 
