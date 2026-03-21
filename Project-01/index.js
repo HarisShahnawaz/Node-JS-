316cb8b21fa401fec863759c0feb1c2c48def595
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-21 23:47:49 +0500

    fix error

--- Project-01/index.js
+++ Project-01/index.js
@@ -5,20 +5,26 @@ const PORT = 8000;
 
 // Routes
 
-app.get("/api/users", (req,res)=> {
-     return  res.json(users)
-})
-
 app.get('/users' , (req,res)=> {
     const html =
     ` <ul>
       ${users.map(user => `<li>${user.first_name}<li>`).join("")}
     <ul>
     `
-
-
     return res.send(html)
 })
+// REST API
+
+app.get("/api/users", (req,res)=> {
+     return  res.json(users)
+})
+app.get("/api/users/:id", (req,res)=> {
+     const id = Number(req.params.id)
+     const user = users.find((user)=> user.id===id)
+     return res.json(user)
+})
+
+
 
 app.listen(PORT,() => console.log(`Server Started at Port ${PORT}`))
 
