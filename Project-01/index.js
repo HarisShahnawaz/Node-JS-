5b4a308ab40a7c1285da2ab0a852c8374aabf9eb
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-23 21:47:57 +0500

    learn about status codes 100-599

--- Project-01/index.js
+++ Project-01/index.js
@@ -39,6 +39,8 @@ app.route('/api/users/:id')
 .get((req,res)=> {
      const id = Number(req.params.id)
      const user = users.find((user)=> user.id===id)
+     if(!user)
+        return res.status(404).json({error:'user not found'})
      return res.json(user)
 })
 .patch((req, res) => {
