1e5a36d2e789df94e533513dc1f21d884381c1b7
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-04-10 18:54:59 +0500

    create connext and mongodb connected

--- Short-url/index.js
+++ Short-url/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
-
+const {connecttoMongoDB} = require('./connect');
+const urlRoute = require('./routes/url');
+const { applyTimestamps } = require('./models/URL');
 const app = express();
 const PORT = 8001;
 
+connecttoMongoDB("mongodb://localhost:27017/short-url").then(() => {
+    console.log("   MongoDB Connected ");
+});
+
+app.use("/url", urlRoute);
 app.listen(PORT, () => console.log(`Server Started at port ${PORT}`));
