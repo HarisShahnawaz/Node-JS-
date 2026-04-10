9ac362d1eb9f7e75fcfd7789ec581f99a9d6bb2f
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-04-10 18:07:03 +0500

    create post route

--- Short-url/controllers/url.js
+++ Short-url/controllers/url.js
@@ -1,3 +1,17 @@
+const {nanoid} = require('nanoid')
+const URL = require('../models/URL')
 async function handleGenerateNewShortURL(req, res) {
-    
+       const body = req.body;
+    if(!body.url) return res.status(400).json({error: 'URL is required'})
+          const shortID = nanoid(8);
+         await URL.create({
+        shortId: shortID,
+        redirectUrl: body.url,
+        visitHistory: [],
+    });
+    return res.json({id : shortID});
+}
+
+module.exports = {
+    handleGenerateNewShortURL
 }
