e8228920f6031351edc44342c70709a1504acbb1
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-04-28 23:30:46 +0500

    fix

--- Short-url/controllers/user.js
+++ Short-url/controllers/user.js
@@ -14,7 +14,7 @@ async function handleUserLogin(req, res) {
 const user =   await User.findOne({email,password});
 console.log('User', user)
    if(!user){
-    return res.render("login" , {error: "Invalid Username or Password"});
+    return res.render("login" , {error: "Invalid Username or password"});
    
 }
  return res.redirect("/");
